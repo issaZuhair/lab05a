e356524b3131524168254597e0e0e21d86c3f362
Issa Al-aiash <izalayyash152@eng.just.edu.jo>
2021-03-05 13:31:15 +0200

    solving the second function

--- app.js
+++ app.js
@@ -29,12 +29,15 @@ Write a function called multiply() that takes in two numbers as arguments and re
 Test this function by hand in the console to get it working, and when you think it is finished, uncomment the call for the testMultiply() function and see if the test passes.*/
 
 // Write your code here
+let multiplicationMatrix=[];
 function multiply(a, b) { //eslint-disable-line
-
+  multiplicationMatrix.push(a*b);
+  multiplicationMatrix.push('The product of ' +a +' and '+b +' is ' +(a*b)+'.');
+  return multiplicationMatrix;
 }
 
 // Here is the test for multiply(); uncomment it to run it
-// testMultiply(5,9);
+testMultiply(5,9);
 
 // Once you get the test passing, do an a-c-p cycle and synchronize the code between GitHub and your laptop. Don't forget to create a new branch for your work on the next question!
 
